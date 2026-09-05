0b58c2095e036b2e7eeef42216e4ab9d99d644f8
bakzhanzhappar-create <bakzhanzhappar@gmail.com>
2026-09-06 00:02:06 +0500

    user db update

--- postgres.py
+++ postgres.py
@@ -1,11 +1,11 @@
 import psycopg
-#Создать отдельный бд, отдельного юзера, продумать структуру адреса, продумать таблицы или условно хорошо все это затестить
+#продумать структуру адреса, продумать таблицы или условно хорошо все это затестить
 conn = psycopg.connect(
     host="localhost",
     port="5432",
-    dbname="baga",
-    user="postgres",
-    password="*password*",
+    dbname="vault_db",
+    user="vault_app",
+    password="2006Bakzhan",
 )
 
 cursor = conn.cursor()
